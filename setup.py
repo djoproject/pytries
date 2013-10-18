9b6bcc63fd13d62f59d34925c3b5d0771b467fe1
Jonathan Delvaux <jdv@aqua>
2013-10-18 15:39:57 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+#Copyright (C) 2012 Jonathan Delvaux <jonathan.delvaux@uclouvain.be>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#http://docs.python.org/2/distutils/setupscript.html
+from distutils.core import setup
+
+setup(name='Pytries',
+      version='1.0',
+      description='Python tries structure',
+      author='Jonathan Delvaux',
+      author_email='djo938@gmail.com',
+      url='https://github.com/djo938/pytries',
+      packages=['tries'],
+     )
+     #license, platforms, 
